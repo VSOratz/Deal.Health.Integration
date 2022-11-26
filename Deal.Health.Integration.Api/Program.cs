4f897fdfaf39a1e9c3badbf08cac0e5542638751
Dougllas Souza <douglas.souza@deal.com.br>
2022-11-26 17:11:03 -0300

    dev_adicao_context

--- Deal.Health.Integration.Api/Program.cs
+++ Deal.Health.Integration.Api/Program.cs
@@ -1,6 +1,15 @@
+using Deal.Health.Integration.Domain.Handlers;
+using Deal.Health.Integration.Domain.Repositories;
+using Deal.Health.Integration.Infra.Data.Context;
+using Deal.Health.Integration.Infra.Data.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<DbContext>();
+builder.Services.AddScoped<IPacienteRepository, PacienteRepository>();
+builder.Services.AddScoped<PacienteHandler>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ Deal.Health.Integration.Infra.Data/Context/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Deal.Health.Integration.Infra.Data.Context
+{
+    public class DbContext: IAsyncDisposable
+    {
+        public MySqlConnection Connection { get; set; }
+
+        public DbContext(IConfiguration configuration)
+        {
+            Connection = new MySqlConnection(configuration.GetConnectionString("Default"));
+            Connection.Open();
+        }
+
+        public async ValueTask DisposeAsync()
+        {
+            await Connection.CloseAsync();
+            await Connection.ClearAllPoolsAsync();
+            await Connection.DisposeAsync();
+        }
+    }
+}
